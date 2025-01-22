51bc4ab5ff5d6450a24bee4e6437e30346a5801b
FilipeFord <fsamaniego9090@gmail.com>
2025-01-22 15:38:20 -0400

    Adicionei uma nova linha

--- numeros.py
+++ numeros.py
@@ -1,17 +1,17 @@
 # 1 - Escreva um programa que receba 2 valoraes do tipo inteiro x e y,
 # e calcule o valor de z: z = (x²+ y²)/(x-y)²
 
-x = int(input("Digite o valor de x: "))
-y = int(input("Digite o valor de y: "))
-z = (x**2 + y ** 2) / (x - y)**2
-print('Z =', z)
+#x = int(input("Digite o valor de x: "))
+#y = int(input("Digite o valor de y: "))
+#z = (x**2 + y ** 2) / (x - y)**2
+#print('Z =', z)
 # 2 - Escreva um programa que receba o salario de um funcionario (float),
 # e retorne o resultado de novo salario com reajuste de 35%
 
-salario = float(input("Informe o seu salário: "))
+#salario = float(input("Informe o seu salário: "))
 
-porcentagem = 35
-aumento = salario * porcentagem / 100
-novo_salario = aumento + salario
+#porcentagem = 35
+#aumento = salario * porcentagem / 100
+#novo_salario = aumento + salario
 
-print(novo_salario)
+#print(novo_salario)
